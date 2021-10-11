db469215158d543ab518b92945d38037e22c6636
ha0372 <ha0372032020@unab.edu.sv>
2021-10-11 16:12:17 -0600

    entidades

---
+++ FastFoodRestaurant/FastFoodRestaurant/Entidades/tbl_Orden.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FastFoodRestaurant.Entidades
+{
+    public class tbl_Orden
+    {
+    }
+}
