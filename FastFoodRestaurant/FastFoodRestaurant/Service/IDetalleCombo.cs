32db987242cf0abfd910f21a5fe091922dd04a71
Kevin2001M <78760245+Kevin2001M@users.noreply.github.com>
2021-10-27 07:12:01 -0600

    Services

---
+++ FastFoodRestaurant/FastFoodRestaurant/Service/IDetalleCombo.cs
@@ -0,0 +1,17 @@
+﻿using FastFoodRestaurant.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FastFoodRestaurant.Service
+{
+    public interface IDetalleCombo
+    {
+        void Insert(tbl_DetalleCombo detalleCombo);
+        void Delete(tbl_DetalleCombo detalleCombo);
+        void Update(tbl_DetalleCombo detalleCombo);
+
+        ICollection<tbl_DetalleCombo> detalleCombo();
+    }
+}
---
+++ FastFoodRestaurant/FastFoodRestaurant/Service/IDocumento.cs
@@ -0,0 +1,17 @@
+﻿using FastFoodRestaurant.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FastFoodRestaurant.Service
+{
+    public interface IDocumento
+    {
+        void Insert(tbl_Documento documento);
+        void Delete(tbl_Documento documento);
+        void Update(tbl_Documento documento);
+
+        ICollection<tbl_Documento> documento();
+    }
+}
