381e03ce14f2224a0da0a51e4ff036144e1722ac
carlosjavierjimenezvasquez <69540480+carlosjavierjimenezvasquez@users.noreply.github.com>
2021-11-02 18:11:40 -0600

    Controller

--- FastFoodRestaurant/FastFoodRestaurant/Controllers/PrincipalController.cs
+++ FastFoodRestaurant/FastFoodRestaurant/Controllers/PrincipalController.cs
@@ -3,11 +3,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FastFoodRestaurant.Service;
+using FastFoodRestaurant.Entidades;
 
 namespace FastFoodRestaurant.Controllers
 {
     public class PrincipalController : Controller
     {
+
+        private IUsuario iusuario;
+
+        public PrincipalController(IUsuario iusuario)
+        {
+
+            this.iusuario = iusuario;
+        }
         public IActionResult Index()
         {
             return View();
@@ -16,6 +26,20 @@ namespace FastFoodRestaurant.Controllers
         public IActionResult LogIn()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Registro(string nombreU,string apellidoU, string correoU, string contraU) 
+        {
+            tbl_User user = new tbl_User();
+            user.nombreU = nombreU;
+            user.apellidoU = apellidoU;
+            user.correoU = correoU;
+            user.contraU = contraU;
+
+            iusuario.Insert(user);
+
+            return Redirect("/Index/Principal");
         }
     }
 }
--- FastFoodRestaurant/FastFoodRestaurant/Startup.cs
+++ FastFoodRestaurant/FastFoodRestaurant/Startup.cs
@@ -1,4 +1,6 @@
 using FastFoodRestaurant.Data;
+using FastFoodRestaurant.Repository;
+using FastFoodRestaurant.Service;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -29,6 +31,7 @@ namespace FastFoodRestaurant
             services.AddDbContext<ApplicationDbContext>(optios =>
            optios.UseSqlServer(Configuration.GetConnectionString
            ("DefaultConection")));
+            services.AddTransient<IUsuario, UsuarioRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
