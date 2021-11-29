efd1ffde704b2d234599fd762348007692d5ec51
Javier14 <69540480+carlosjavierjimenezvasquez@users.noreply.github.com>
2021-11-29 17:53:54 -0600

    AdministrarP

---
+++ RestaurantOnline/RestaurantOnline/Controllers/AgregarPController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantOnline.Data;
+using RestaurantOnline.Entidades;
+using RestaurantOnline.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantOnline.Controllers
+{
+    public class AgregarPController : Controller
+    {
+
+        private ApplicationDbContext db;
+        private IProducto iproducto;
+
+        public AgregarPController(ApplicationDbContext db, IProducto iproducto)
+        {
+            this.db = db;
+            this.iproducto = iproducto;
+        }
+
+        public IActionResult AgregarProductos()
+        {
+            var productos = iproducto.ListofP();
+            return View(productos);
+        }
+
+        public IActionResult Save(tbl_Producto productos)
+        {
+
+            tbl_Producto producto = new tbl_Producto();
+
+
+            if (productos.producto_id == 0)
+            {
+                producto.nombreProducto = productos.nombreProducto;
+                producto.precioP = productos.precioP;
+                producto.fechaCreacionP = DateTime.Now;
+                producto.estadoProducto = "activo";
+                producto.imageP = productos.imageP;
+                producto.menu_Fk = productos.menu_Fk;
+
+                iproducto.Insert(producto);
+            }
+            else
+            {
+                int update = productos.producto_id;
+                tbl_Producto editar = db.tbl_Producto.Where(x => x.producto_id == update).FirstOrDefault();
+                editar.nombreProducto = productos.nombreProducto;
+                editar.precioP = productos.precioP;
+                editar.estadoProducto = productos.estadoProducto;
+                editar.imageP = productos.imageP;
+                editar.menu_Fk = productos.menu_Fk;
+
+                iproducto.Update(editar);
+            }
+
+     
+                return Redirect("/AgregarP/AgregarProductos");
+           
+            
+
+        }
+
+        public IActionResult DatosProductos( tbl_Producto productos)
+        {
+            ViewBag.productoid = productos.producto_id;
+            ViewBag.nombreProducto = productos.nombreProducto;
+            ViewBag.preciop = productos.precioP;
+            ViewBag.fechaProducto = productos.fechaCreacionP;
+            ViewBag.estadoProducto = productos.estadoProducto;
+            ViewBag.imageP = productos.imageP;
+            ViewBag.menuFK = productos.menu_Fk;
+
+
+            return View("DatosProductos");
+        }
+    }
+}
